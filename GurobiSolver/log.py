0530082cc57a01fcb0c7a962fc445b831334de17
Jonathan Yip <jonathangyip@gmail.com>
2017-12-03 16:35:28 -0800

    Added comments to Log.

--- GurobiSolver/log.py
+++ GurobiSolver/log.py
@@ -15,12 +15,36 @@ class Log(object):
 
     @staticmethod
     def println(namespace, string, color=Fore.MAGENTA):
+        """
+        Prints string tagged with a namespace with some colors
+        Args:
+            namespace: A string stating what part of the app is logging this
+            string: The string to print
+        Kwargs:
+            color: The color to make the namespace tag (Log.colors)
+        """
         print("[{}{}{}] {}".format(color, namespace, Fore.RESET, string))
 
     @staticmethod
     def error(namespace, string, color=Fore.MAGENTA):
+        """
+        Prints string to stderr tagged with a namespace with some colors
+        Args:
+            namespace: A string stating what part of the app is logging this
+            string: The string to print
+        Kwargs:
+            color: The color to make the namespace tag (Log.colors)
+        """
         sys.stderr.write("[{}{}{}:{}ERROR{}] {}\n".format(color, namespace, Fore.RESET, Fore.RED, Fore.RESET, string))
 
     @staticmethod
     def write(namespace, string, color=Fore.MAGENTA):
+        """
+        Prints string with no newline tagged with a namespace with some colors
+        Args:
+            namespace: A string stating what part of the app is logging this
+            string: The string to print
+        Kwargs:
+            color: The color to make the namespace tag (Log.colors)
+        """
         sys.stdout.write("[{}{}{}] {}".format(color, namespace, Fore.RESET, string))
